b0ed927d7242c4a8711c00e26af800876c34a88a
dilan-flores <117755180+dilan-flores@users.noreply.github.com>
2023-02-05 22:11:54 -0500

    Correción menú: Nombre y foto de usuario

--- src/Menu.java
+++ src/Menu.java
@@ -86,13 +86,18 @@ public class Menu extends JFrame implements ActionListener{
         }
     }
 
-    public static void main(String[] args) {
+    public void a() {
         Menu m1 = new Menu();
         m1.setBounds(0,0,550,550);
         m1.setLocationRelativeTo(null);
         m1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         m1.setVisible(true);
 
+        JLabel Usuario = new JLabel();
+        Usuario.setText("Miguel");
+        Usuario.setBounds(10,270,100,100);
+        Usuario.setVisible(true);
+        m1.add(Usuario);
 
         JLabel label = new JLabel();
         m1.add(label);
